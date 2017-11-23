7acfc5857991b7ed80275121852c555d32e0e7fc
Claudia Latella <claudia.latella.86@gmail.com>
2017-11-23 11:03:47 +0100

    Modified the transformation through the use of iDynTree adjoint

--- matlab_calibShoes/change_sor.m
+++ matlab_calibShoes/change_sor.m
@@ -4,16 +4,10 @@ function [ outdata ] = change_sor( indata, params )
         for d=1 : length(devs)
             outdata.(devs{d}).('time') = indata.(devs{d}).('time');
             outdata.(devs{d}).('data') = zeros(size(indata.(devs{d}).('data')));
-            forces = indata.(devs{d}).('data')(:,1:3);
-            moments = indata.(devs{d}).('data')(:,4:6);
-            forcesInFtsSoR = ((params.amtiInFts_rot{d})'*forces')';
-            outdata.(devs{d}).('data')(:,1:3) = forcesInFtsSoR;
-            tmp = zeros(size(moments));
-            for t =1:length(moments)
-                tmp(t,:) = cross((params.amtiInFts_trasl{d})', forcesInFtsSoR(t,:)')';
-            end
             
-            momentsInFtsSoR = ((params.amtiInFts_rot{d})'* moments')' + tmp;
-            outdata.(devs{d}).('data')(:,4:6) = momentsInFtsSoR;
+            for t = 1 : size(indata.(devs{d}).('data'),1)
+                data_sample = indata.(devs{d}).('data')(t,:);
+                outdata.(devs{d}).('data')(t,:) = (params.fts_T_fp{d}.asAdjointTransformWrench().toMatlab()*data_sample')';
+            end
         end
 end
